d5c62d7ae20a4409afc92e32ad380f5f8774b975
Camila Iglesia <camila.iglesia@yahoo.com.ar>
2023-03-13 15:37:11 -0300

    Creando plantillas heredadas

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from AppCoder.views import *
 
 urlpatterns = [
-    path("cursos", cursos),
-    path("estudiantes", estudiantes),
-    path("profesores", profesores)
+    path("cursos", cursos, name="AppCoderCursos"),
+    path("estudiantes", estudiantes, name="AppCoderEstudiantes"),
+    path("profesores", profesores, name="AppCoderProfesores"),
+    path("curso/<nombre>/<camada>", crear_curso,name="AppCoderCurso"),
+    
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -4,10 +4,22 @@ from AppCoder.models import Curso
 
 # Create your views here.
 def cursos(request):
-    return render(request, "index.html")
+    all_cursos = Curso.objects.all()
+    context= {
+        "cursos" : all_cursos
+    }
+    return render(request, "AppCoder/cursos.html", context=context)
+
+def crear_curso(request,nombre,camada):
+    save_curso= Curso(nombre=nombre, camada= int(camada))
+    save_curso.save()
+    context= { 
+        "nombre": nombre
+    }
+    return render(request, "AppCoder/save_curso.html", context)
 
 def estudiantes(request):
-    return render(request, "index.html")
+    return render(request, "base.html")
 
 def profesores(request):
-    return render(request, "index.html")
+    return render(request, "base.html")
